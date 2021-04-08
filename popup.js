4090c13996e8826e83f6ebe9b34e27bb06c5b0a2
TaiTuLangQuen <duycong.cn@gmail.com>
2021-04-08 18:02:11 +0700

    test cho vui nao

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('checkPage');
+    checkPageButton.addEventListener('click', function() {
+        var data = document.createElement('span');
+        data.style.cssText = 'font-size:14px; font-weight: bold; background-image:linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red); -webkit-background-clip:text; color:transparent;'
+        var node = document.createTextNode("Toang :)");
+        data.appendChild(node);
+        document.body.appendChild(data);
+    });
+});
