6d22bb2d08fac467dec31ed0574d562a3842adf0
TaiTuLangQuen <duycong.cn@gmail.com>
2021-04-08 18:09:57 +0700

    toang:)

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     checkPageButton.addEventListener('click', function() {
         var data = document.createElement('span');
         data.style.cssText = 'font-size:14px; font-weight: bold; background-image:linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red); -webkit-background-clip:text; color:transparent;'
-        var node = document.createTextNode("Toang :)");
+        var node = document.createTextNode("Toang:) ");
         data.appendChild(node);
         document.body.appendChild(data);
     });
