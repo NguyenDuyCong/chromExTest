d7b4f168b2da9f7d32b5005c99b86cd60b4daf28
TaiTuLangQuen <duycong.cn@gmail.com>
2021-04-09 19:53:47 +0700

    update text

--- scripts/popup.js
+++ scripts/popup.js
@@ -11,17 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
         p1.appendChild(node1);
         document.body.appendChild(p1);
         
-        // var p2 = document.createElement('p');
-        // p2.classList.add('toang')
-        // var node2 = document.createTextNode("Bấm làm cái gì");
-        // p2.appendChild(node2);
-        // document.body.appendChild(p2);
-
         var p3 = document.createElement('p');
         p3.classList.add('toang')
         var node3 = document.createTextNode("Chán thì đi chơi đi");
         p3.appendChild(node3);
         document.body.appendChild(p3);
+        
+        var p2 = document.createElement('p');
+        p2.classList.add('toang')
+        var node2 = document.createTextNode("Gà thế :))");
+        p2.appendChild(node2);
+        document.body.appendChild(p2);
 
         var img = document.createElement('div');
         img.classList.add('image');
