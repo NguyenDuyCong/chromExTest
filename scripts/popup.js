5ff51ef256a97dd41e5a1716cc7294a09ddf935d
TaiTuLangQuen <duycong.cn@gmail.com>
2021-04-09 19:56:48 +0700

    update text part 2

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,6 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
         var text = document.getElementById('text');
         text.parentNode.removeChild(text);
         
+        var p0 = document.createElement('p');
+        p0.classList.add('toang')
+        var node0 = document.createTextNode("Không làm mà... À");
+        p0.appendChild(node0);
+        document.body.appendChild(p0);
+
         var p1 = document.createElement('p');
         p1.classList.add('toang')
         var node1 = document.createTextNode("Éo có gì đâu mà bấm");
