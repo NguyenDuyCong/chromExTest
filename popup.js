4e051830a5135426895725b8b68772f1f7c51841
TaiTuLangQuen <duycong.cn@gmail.com>
2021-04-09 19:31:46 +0700

    update phiên bản cú lừa

--- popup.js
+++ popup.js
@@ -1,10 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('checkPage');
     checkPageButton.addEventListener('click', function() {
-        var data = document.createElement('span');
-        data.style.cssText = 'font-size:14px; font-weight: bold; background-image:linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red); -webkit-background-clip:text; color:transparent;'
-        var node = document.createTextNode("Toang:) ");
-        data.appendChild(node);
-        document.body.appendChild(data);
+        checkPageButton.parentNode.removeChild(checkPageButton);
+        var text = document.getElementById('text');
+        text.parentNode.removeChild(text);
+        
+        var p1 = document.createElement('p');
+        p1.classList.add('toang')
+        var node1 = document.createTextNode("Éo có gì đâu mà bấm");
+        p1.appendChild(node1);
+        document.body.appendChild(p1);
+        
+        // var p2 = document.createElement('p');
+        // p2.classList.add('toang')
+        // var node2 = document.createTextNode("Bấm làm cái gì");
+        // p2.appendChild(node2);
+        // document.body.appendChild(p2);
+
+        var p3 = document.createElement('p');
+        p3.classList.add('toang')
+        var node3 = document.createTextNode("Chán thì đi chơi đi");
+        p3.appendChild(node3);
+        document.body.appendChild(p3);
+
+        var img = document.createElement('div');
+        img.classList.add('image');
+        document.body.appendChild(img);
     });
 });
